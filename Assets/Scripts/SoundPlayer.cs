34db2514a69441bcd2b8c9853452eaed1b41671e
8eastwood <m.fishborn@yandex.ru>
2024-12-04 18:30:23 +0300

    init

--- Assets/Scripts/SoundPlayer.cs
+++ Assets/Scripts/SoundPlayer.cs
@@ -4,20 +4,36 @@ using UnityEngine.UI;
 
 public class SoundPlayer : MonoBehaviour
 {
-    [SerializeField] private Button _elephantButton;
-    [SerializeField] private Button _engineButton;
-    [SerializeField] private Button _rainButton;
+    [SerializeField] private AudioMixerGroup _mixer;
     [SerializeField] private AudioSource _backgroundMusic;
     [SerializeField] private AudioSource _elephantSound;
     [SerializeField] private AudioSource _engineSound;
     [SerializeField] private AudioSource _rainSound;
-    [SerializeField] private AudioMixerGroup _mixer;
+    [SerializeField] private Button _elephantButton;
+    [SerializeField] private Button _engineButton;
+    [SerializeField] private Button _rainButton;
 
+    private string _engineVolumeParameter = "EngineVolume";
+    private string _elephantVolumeParameter = "ElephantVolume";
+    private string _rainVolumeParameter = "RainVolume";
+    private string _musicVolumeParameter = "MusicVolume";
+    private string _masterVolumeParameter = "MasterVolume";
     private bool _isSoundsPlaying = false;
+    private int _lowestValue = -80;
+    private int _defaultValue = 0;
+    private int _coefficient = 20;
 
     private void Awake()
     {
         _backgroundMusic.Play();
+    }
+
+    private void Update()
+    {
+        if (_isSoundsPlaying)
+        {
+            _backgroundMusic.Stop();
+        }
     }
 
     public void OnEnable()
@@ -36,26 +52,51 @@ public class SoundPlayer : MonoBehaviour
 
     public void ToggleMusic(bool enabled)
     {
-
+        if (enabled)
+        {
+            _mixer.audioMixer.SetFloat(_musicVolumeParameter, _defaultValue);
+        }
+        else
+        {
+            _mixer.audioMixer.SetFloat(_musicVolumeParameter, _lowestValue);
+        }
     }
 
-    public void ChangeVolume(float volume)
+    public void ChangeEngineVolume(float soundLevel)
     {
+        _mixer.audioMixer.SetFloat(_engineVolumeParameter, Mathf.Log10(soundLevel) * _coefficient);
+    }
 
+    public void ChangeElephantVolume(float soundLevel)
+    {
+        _mixer.audioMixer.SetFloat(_elephantVolumeParameter, Mathf.Log10(soundLevel) * _coefficient);
+    }
+
+    public void ChangeRainVolume(float soundLevel)
+    {
+        _mixer.audioMixer.SetFloat(_rainVolumeParameter, Mathf.Log10(soundLevel) * _coefficient);
+    }
+
+    public void ChangeMasterVolume(float soundLevel)
+    {
+        _mixer.audioMixer.SetFloat(_masterVolumeParameter, Mathf.Log10(soundLevel) * _coefficient);
     }
 
     private void OnElephantButtonClick()
     {
+        _isSoundsPlaying = true;
         _elephantSound.Play();
     }
 
     private void OnRainButtonClick()
     {
+        _isSoundsPlaying = true;
         _rainSound.Play();
     }
 
     private void OnEngineButtonClick()
     {
+        _isSoundsPlaying = true;
         _engineSound.Play();
     }
 }
